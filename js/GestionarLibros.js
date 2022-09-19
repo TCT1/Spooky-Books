0c788c3804582f4a756890ec34c6a93e79dac2ac
Leonardo JV <leonjv08@gmail.com>
2022-09-18 23:00:09 -0500

    Se corrigió la funcionalidad de la página

--- js/GestionarLibros.js
+++ js/GestionarLibros.js
@@ -9,18 +9,9 @@ class GestionarLibros{
         })
     }
 
-    cargarLibros(){
-        let contenedorLibrosCatalogo = document.getElementById("Catalogo")        
-        productosMostrar.forEach( (producto) => {
-            let prod = document.createElement('div');
-            prod.classList.add('Producto');  
-            prod.innerHTML = `
-                <img class="Producto__imagen" src="${producto.img}">
-                <span class="Producto__descripcion">${producto.descripcion}</span>
-                <span class="Producto__precio">$${producto.precio}</span>
-                <ion-icon id="btn" name="cart"></ion-icon>
-            `;
-            contenedorLibrosCatalogo.appendChild(prod);
-        }) 
+    cargarLibros(){     
+        productosMostrar.forEach((producto) => {
+            document.getElementById("Producto__imagen").src=`${producto.img}`;
+        })
     }
 }
